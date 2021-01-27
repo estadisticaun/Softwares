abc3de74e0dc6ffafe3cd2e9955a3f581fcc4b4b
CamilaAcostaRamirez <48154345+CamilaAcostaRamirez@users.noreply.github.com>
2021-01-27 14:23:33 -0500

    Forma de navegación

--- dataflourish.R
+++ dataflourish.R
@@ -8,18 +8,39 @@ evoluciongeneral <- Graduados %>% group_by(YEAR_SEMESTER) %>% count()
 write.csv(evoluciongeneral, file = "evoluciongeneral.csv", row.names = F)
 
 #GRAFICO DE LINEAS SEGMNETADO POR UNA DIMENSION (MODALIDAD DE FORMACION)
-modalidadfromacion <- Graduados %>% group_by(YEAR_SEMESTER, TIPO_NIVEL) %>% 
+modalidadformacion <- Graduados %>% group_by(YEAR_SEMESTER, TIPO_NIVEL) %>% 
   count() %>% 
   spread(key = TIPO_NIVEL, value = n) %>% 
-  mutate(Porcentajepregrado = (Pregrado/(Pregrado + Postgrado)*100)) %>% 
-  mutate(Porcentajepostgrado = (Postgrado/(Pregrado + Postgrado)*100))
-write.csv(modalidadfromacion, file = "modalidadfromacion.csv", row.names = F)
+  mutate(Porcentajepregrado = round((Pregrado/(Pregrado + Postgrado)*100), 2)) %>% 
+  mutate(Porcentajepostgrado = round((Postgrado/(Pregrado + Postgrado)*100),2))
+write.csv(modalidadformacion, file = "modalidadformacion.csv", row.names = F)
 
-#GRAFICO DE LINEAS SEGMNETADO POR UNA DIMENSION (NIVEL DE FORMACION)
-nivelfromacion <- Graduados %>% group_by(YEAR_SEMESTER, NIVEL) %>% 
-  count() %>% spread(key = NIVEL, value = n) %>% 
-  mutate(Porcentajepregrado = (Pregrado/(Pregrado + Postgrado)*100)) %>% 
-  mutate(Porcentajepostgrado = (Postgrado/(Pregrado + Postgrado)*100)) %>% 
-  mutate(Porcentajepregrado = (Pregrado/(Pregrado + Postgrado)*100)) %>% 
-  mutate(Porcentajepostgrado = (Postgrado/(Pregrado + Postgrado)*100)) %>% 
-write.csv(nivelfromacion, file = "nivelfromacion.csv", row.names = F)
+#TABLA (MODALIDAD DE FORMACION)
+modalidadformaciontabla <- Graduados %>% group_by(YEAR, SEMESTRE, TIPO_NIVEL) %>% 
+  count() %>% 
+  spread(key = TIPO_NIVEL, value = n) %>% 
+  mutate(Total = Pregrado + Postgrado) %>% 
+  mutate(Porcentajepregrado = round((Pregrado/(Pregrado + Postgrado)*100), 2)) %>% 
+  mutate(Porcentajepostgrado = round((Postgrado/(Pregrado + Postgrado)*100),2))
+write.csv(modalidadformaciontabla, file = "modalidadformaciontabla.csv", 
+          row.names = F)
+
+#GRAFICO CIRCULAR (MODALIDAD DE FORMACION) CON VARIABLE YEAR_SEMESTER
+modalidadformacioncircular <- Graduados %>% 
+  group_by(YEAR_SEMESTER, TIPO_NIVEL) %>% 
+  mutate(YEAR_SEMESTER = as.factor(YEAR_SEMESTER)) %>% 
+  summarise(Cantidad = n()) %>% 
+  mutate(Total = aggregate(Cantidad~YEAR_SEMESTER, FUN = sum)[,2]) %>%  
+  mutate(Porcentaje = round((Cantidad/Total)*100, 2))
+write.csv(modalidadformacioncircular, file = "modalidadformacioncircular.csv", 
+          row.names = F)
+
+#FILTRAR POR PERIODO 2020-1
+modalidadformacioncircularfiltrada <- Graduados %>% 
+  group_by(YEAR_SEMESTER, TIPO_NIVEL) %>% 
+  summarise(Cantidad = n()) %>% 
+  filter(YEAR_SEMESTER == "2020 - 1") %>% 
+  mutate(Porcetaje = round((Cantidad/sum(Cantidad))*100, 2))
+write.csv(modalidadformacioncircularfiltrada, 
+          file = "modalidadformacioncircularfiltrada.csv", 
+          row.names = F)
